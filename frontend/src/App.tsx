de50f18f1d6e5d0c721fd1377094d92776d6d994
Diego Lima <diiserras@gmail.com>
2022-05-14 15:51:00 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import HeaderBar from "./components/HeaderBar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import HeaderBar from "components/HeaderBar";
 
 function App() {
   return (
-    <>
-      <HeaderBar/>
-    </>
+    <BrowserRouter>
+      <HeaderBar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
