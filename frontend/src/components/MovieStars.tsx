438f8c252875f348c141155fe9c29075cfb544b3
Diego Lima <diiserras@gmail.com>
2022-05-20 23:01:34 -0300

    Show score

--- frontend/src/components/MovieStars.tsx
+++ frontend/src/components/MovieStars.tsx
@@ -1,16 +1,51 @@
 import { ReactComponent as StarFull } from "assets/starfull.svg";
 import { ReactComponent as StarEmpty } from "assets/starempty.svg";
 import { ReactComponent as StarHalf } from "assets/starhalf.svg";
-import 'styles/MovieStars.css'
+import "styles/MovieStars.css";
 
-export default function MovieStars() {
+type Props = {
+  score: number;
+};
+
+type FillProps = {
+  fill: number;
+};
+// EX:
+// getFills(3.5) => [1, 1, 1, 0.5, 0]
+// getFills(4.1) => [1, 1, 1, 1, 0.5]
+function getFills(score: number) {
+  const fills = [0, 0, 0, 0, 0];
+  const integerPart = Math.floor(score);
+
+  for (let i = 0; i < integerPart; i++) {
+    fills[i] = 1;
+  }
+
+  const diff = score - integerPart;
+  if (diff > 0) {
+    fills[integerPart] = 0.5;
+  }
+
+  return fills;
+}
+
+function Star({fill}: FillProps) {
+  if (fill === 0) {
+    return <StarEmpty />
+  } else if (fill === 1){
+    return <StarFull />
+  } else {
+    return <StarHalf />
+  }
+}
+
+export default function MovieStars({ score }: Props) {
+
+  const fills = getFills(score);
+
   return (
     <div className="dsmovie-stars-container">
-      <StarFull />
-      <StarFull />
-      <StarFull />
-      <StarHalf />
-      <StarEmpty />
+      {fills.map(item => <Star fill={item}/>)}
     </div>
   );
 }
